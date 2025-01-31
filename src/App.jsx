9747bc88eb04cb970248669b6d1cc0bf568dcb93
MohsenKazemizade <mohsenkazemizade.dev@gmail.com>
2025-01-31 18:06:04 +0330

    feat:done with header but not the navigation menu for now till have some js:

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <Router>
       <div className="App_app__J98qy " id="app">
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
