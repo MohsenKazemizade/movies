3ef8615f88286ae81db5fb82279d9c638069ab9e
MohsenKazemizade <mohsenkazemizade.dev@gmail.com>
2025-02-03 14:06:37 +0330

    feat: hero section done with default image later it going to be slid show

--- src/components/HomeHero/Hero.jsx
+++ src/components/HomeHero/Hero.jsx
@@ -13,6 +13,64 @@ const Hero = () => {
           <div className="bitmovinplayer-poster"></div>
         </div>
       </div>
+      <div className="container">
+        <div className="hero_hero__metadata__nrUiw">
+          <div></div>
+          <h1 aria-label="Featured Title: Follow Me">Follow Me</h1>
+          <div className="hero_hero__ratings-tags__VqLU7">
+            <p aria-label="Featured Rating: Not Rated">Not Rated</p>
+            <ul>
+              <li aria-label="Featured Genre: Competition">Competition</li>
+              <li aria-label="Featured Genre: Reality Show">Reality Show</li>
+            </ul>
+          </div>
+          <div className="hero_hero__collapsible-container__qG_Y_">
+            <div className="hero_hero__buttons__fKXVK">
+              <button
+                className="button-base_outer__lo8Kr button-base_md-48-16__DWmaI button-base_primary__2nq0a  button-base_solid__rqePh     "
+                aria-label="Featured Follow Me, Watch S2:E1, "
+                to="/watch/6e95dc0d-c487-41d4-80e0-aac19b724103/follow-me?seriesId=236b402c-1298-41fc-b12f-604b2eb2887b"
+                type="button"
+              >
+                <div className="button-base_inner__k9W0g">
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 27.963 24"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M 26.484 9.609 L 7.824 0.28 C 7.422 0.078 6.974 -0.017 6.524 0.003 C 6.074 0.023 5.637 0.157 5.254 0.394 C 4.871 0.631 4.554 0.962 4.335 1.355 C 4.116 1.749 4.001 2.192 4.002 2.642 L 4.002 21.358 C 4.001 21.808 4.116 22.251 4.335 22.645 C 4.554 23.038 4.871 23.369 5.254 23.606 C 5.637 23.843 6.074 23.977 6.524 23.997 C 6.974 24.017 7.422 23.922 7.824 23.72 L 26.484 14.4 C 26.928 14.178 27.302 13.837 27.563 13.414 C 27.825 12.992 27.963 12.505 27.963 12.009 C 27.963 11.512 27.825 11.025 27.563 10.603 C 27.302 10.18 26.928 9.839 26.484 9.617 L 26.484 9.609 Z"></path>
+                  </svg>
+                  <span>
+                    <div aria-hidden="true">Watch S2:E1</div>
+                  </span>
+                </div>
+              </button>
+              <button
+                className="button-base_outer__lo8Kr button-base_md-48-16__DWmaI button-base_primary__2nq0a  button-base_transparent__d3kp_     "
+                aria-label="Featured More Info"
+                to="/details/6e95dc0d-c487-41d4-80e0-aac19b724103/follow-me"
+                type="button"
+              >
+                <div className="button-base_inner__k9W0g">
+                  <span>
+                    <div aria-hidden="true">More Info</div>
+                  </span>
+                </div>
+              </button>
+            </div>
+            <div className="hero_hero__description__q1KE5">
+              <p aria-label="Featured Description: NEW EPISODES! Follow Me, hosted by Bethany Mota, features rising influencers competing to be the next social media star. Famous guest judges coach the contestants as they try to win the $50,000 prize!">
+                NEW EPISODES! Follow Me, hosted by Bethany Mota, features rising
+                influencers competing to be the next social media star. Famous
+                guest judges coach the contestants as they try to win the
+                $50,000 prize!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
