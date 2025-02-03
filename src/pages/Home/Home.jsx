c8edb09293780121a273cc91e770a984d0a65919
MohsenKazemizade <mohsenkazemizade.dev@gmail.com>
2025-02-03 12:41:00 +0330

    main: main page defualt style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,7 @@ import "./Home.css";
 const Home = () => {
   return (
     <main className="base-container_main__mvaf5" id="base-container">
-      <section className="hero">
-        <div className="hero-content">
-          <h1>Welcome to Movie Website</h1>
-          <p>Discover the latest movies and TV shows.</p>
-        </div>
-      </section>
-      <section className="movie-grid">{/* Movie cards will go here */}</section>
+      <div className="home_home__DKDa2"></div>
     </main>
   );
 };
