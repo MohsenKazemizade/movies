fd425b5df3c7a5871f2f6e4b78c4ebe85c664c58
MohsenKazemizade <mohsenkazemizade.dev@gmail.com>
2025-02-03 14:34:41 +0330

    feat: basic style and html for movieRow section

--- src/components/main/MovieRow/MovieRow.jsx
+++ src/components/main/MovieRow/MovieRow.jsx
@@ -1,7 +1,18 @@
+import "./MovieRow.css";
 const MovieRow = () => {
   return (
-    <div>
-      <h1>MovieRow</h1>
+    <div className="row">
+      <div
+        aria-label="Spotlight"
+        className="content-row_content-row__TxcaA content-row_content-row__scrolling__UH8_7  content-row_atBegining__jjcPt content-row_standard__l5zjC"
+      >
+        <div className="container content-row_content-row__title__gZklP"></div>
+        <div className="content-row_content-row__carousel__J2xRM undefined larger-cards"></div>
+        <div
+          className="content-row_content-row__arrow__74W_d"
+          style={{ display: "block" }}
+        ></div>
+      </div>
     </div>
   );
 };
